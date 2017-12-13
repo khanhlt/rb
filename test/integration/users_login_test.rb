1bb425b050570099daf6fe76eff2e13cbf4696b4
khanhlt <ltk.hust@gmail.com>
2017-12-13 10:18:07 +0700

    sign-up/log-in/log-out

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -49,7 +49,7 @@
 
   	  test "login with remembering" do
 	    log_in_as(@user, remember_me: '1')
-	    assert_equal FILL_IN, assigns(:user).FILL_IN
+	    assert_not_nil cookies['remember_token']
      end
 
 	  test "login without remembering" do
